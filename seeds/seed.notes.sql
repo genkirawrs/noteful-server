3a77b718ef05160c4d1abf8d0e34302d5e67ac61
Rosalind H <rosalind.hung99@gmail.com>
2020-06-27 00:03:54 -0700

    for wdt 15.19

---
+++ seeds/seed.notes.sql
@@ -0,0 +1,16 @@
+INSERT INTO noteful_notes (title, content, folder_id)
+VALUES
+  ('Dogs','woof woof woof', 1),
+  ('Cats','meow meow meow meow', 2),
+  ('Pigs','oink oink oink', 3),
+  ('Birds','chirp chirp', 1),
+  ('Bears','rawr?', 2),
+  ('Horses','neigh!', 3),
+  ('Tigers','purrrr', 1),
+  ('Wolves','awooooo', 2),
+  ('Elephants','brrrrrt!!', 3),
+  ('Lamb','baaaaah', 1),
+  ('Owls','hoot hoot', 2),
+  ('Rooster','Cock a doodle doooo', 3),
+  ('Turtles','snap snap snap', 1),
+  ('Cow','moooo', 3);
